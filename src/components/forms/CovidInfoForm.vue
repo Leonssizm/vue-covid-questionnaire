2248d6ac6f1f0adfdaf75e2a07f1e090be087d57
Leonssizm <levan.kereselidze@redberry.ge>
2023-05-10 14:56:45 +0400

    feat: form validations

--- src/components/forms/CovidInfoForm.vue
+++ src/components/forms/CovidInfoForm.vue
@@ -6,7 +6,7 @@
           <label
             for="name"
             class="block text-xl font-bold mb-3 leading-7"
-            :class="{ 'text-red-500': covidQuestion }"
+            :class="{ 'text-normal': covidQuestion }"
             >გაქვთ გადატანილი Covid-19*</label
           >
           <div class="flex flex-col ml-6">
@@ -27,6 +27,7 @@
                 type="radio"
                 name="covid"
                 value="covid-no"
+                rules="required"
                 class="mr-2 w-5 accent-black"
                 @click="
                   ;(showAntiBodiesQuestion = false), (showTestDate = false), (showCovidDate = false)
@@ -40,6 +41,7 @@
                 type="radio"
                 name="covid"
                 value="covid-now"
+                rules="required"
                 class="mr-2 w-5 accent-black"
                 @click="
                   ;(showAntiBodiesQuestion = false), (showTestDate = false), (showCovidDate = false)
@@ -56,22 +58,26 @@
             >
             <div class="flex flex-col ml-6">
               <div class="flex">
-                <input
+                <Field
                   type="radio"
                   name="antibodies"
                   value="antibodies-yes"
+                  rules="required"
                   class="mr-2 w-5 accent-black"
                   @click=";(showTestDate = true), (showCovidDate = false)"
+                  v-model="antibodiesQuestion"
                 />
                 <span class="ml-4 leading-7">კი</span>
               </div>
               <div class="flex mt-2">
-                <input
+                <Field
                   type="radio"
                   name="antibodies"
                   value="antibodies-no"
+                  rules="required"
                   class="mr-2 w-5 accent-black"
                   @click=";(showCovidDate = true), (showTestDate = false)"
+                  v-model="antibodiesQuestion"
                 />
                 <span class="ml-4 leading-7">არა</span>
               </div>
@@ -83,18 +89,24 @@
               >თუ გახსოვს, გთხოვ მიუთითე ტესტის მიახლოებითი <br />რიცხვი და ანტისხეულების
               რაოდენობა*</label
             >
-            <input
+            <Field
               :type="inputType"
               placeholder="რიცხვი"
+              name="antibodiesTestDate"
+              rules="required"
               class="py-3 px-4 border border-black w-8/12 bg-[#EAEAEA] placeholder:text-[#232323] font-normal text-xs ml-6 mt-8"
               @blur="inputType = 'text'"
               @focus="inputType = 'date'"
+              v-model="antibodiesTestDate"
             />
 
-            <input
+            <Field
               type="text"
               placeholder="ანტისხეულების რაოდენობა"
+              rules="required"
+              name="antibodiesAmount"
               class="py-3 px-4 border border-black w-8/12 bg-[#EAEAEA] placeholder:text-[#232323] font-normal text-xs ml-6 mt-4"
+              v-model="antibodiesAmount"
             />
           </div>
 
@@ -103,12 +115,15 @@
               >მიუთითე მიახლოებითი პერიოდი(დღე/თვე/წელი) <br />
               როდის გქონდა Covid-19*</label
             >
-            <input
+            <Field
               :type="inputType"
               placeholder="დდ/თთ/წწ"
+              rules="required"
+              name="hadCovidAtDate"
               class="py-3 px-4 border border-black w-8/12 bg-[#EAEAEA] placeholder:text-[#232323] font-normal text-xs ml-6 mt-8"
               @blur="inputType = 'text'"
               @focus="inputType = 'date'"
+              v-model="hadCovidAtDate"
             />
           </div>
         </div>
@@ -130,18 +145,60 @@
 import IconNext from '@/components/icons/IconNext.vue'
 import IconPrevious from '@/components/icons/IconPrevious.vue'
 import { Form, Field } from 'vee-validate'
-import { ref } from 'vue'
+import { ref, onUnmounted, onMounted } from 'vue'
 import { RouterLink, useRouter } from 'vue-router'
 
+const router = useRouter()
 let showAntiBodiesQuestion = ref(false)
 let showTestDate = ref(false)
 let showCovidDate = ref(false)
 let inputType = ref('text')
 
 let covidQuestion = ref('')
-const router = useRouter()
+let antibodiesQuestion = ref('')
+let antibodiesTestDate = ref('')
+let antibodiesAmount = ref('')
+let hadCovidAtDate = ref('')
+
+onMounted(() => {
+  window.onbeforeunload = function () {
+    storeFormDataInLocalStorage()
+  }
+  if (localStorage.getItem('covid-info') !== null) {
+    covidQuestion.value = JSON.parse(localStorage.getItem('covid-info')).covidQuestion
+    if (covidQuestion.value == 'covid-yes') {
+      showAntiBodiesQuestion.value = true
+      antibodiesQuestion.value = JSON.parse(localStorage.getItem('covid-info')).antibodiesQuestion
+      if (antibodiesQuestion.value == 'antibodies-yes') {
+        showTestDate.value = true
+        antibodiesTestDate.value = JSON.parse(localStorage.getItem('covid-info')).antibodiesTestDate
+        antibodiesAmount.value = JSON.parse(localStorage.getItem('covid-info')).antibodiesAmount
+      } else {
+        hadCovidAtDate.value = JSON.parse(localStorage.getItem('covid-info')).hadCovidAtDate
+        showCovidDate.value = true
+      }
+    }
+  }
+})
+
+onUnmounted(() => {
+  storeFormDataInLocalStorage()
+})
 
 function handleHadCovidForm() {
   router.push({ name: 'vaccination-info' })
+}
+
+function storeFormDataInLocalStorage() {
+  localStorage.setItem(
+    'covid-info',
+    JSON.stringify({
+      covidQuestion: covidQuestion.value,
+      antibodiesQuestion: antibodiesQuestion.value,
+      antibodiesTestDate: antibodiesTestDate.value,
+      antibodiesAmount: antibodiesAmount.value,
+      hadCovidAtDate: hadCovidAtDate.value
+    })
+  )
 }
 </script>
